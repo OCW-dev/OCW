a0c297d4e62f3802a930d316a056d7d4927b4ff2
rey256 <medvedosandr@gmail.com>
2024-09-19 14:48:20 +0300

    .

---
+++ app/assets/scripts/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const walletOption = document.getElementById("wallet-option");
+  const contestOption = document.getElementById("contest-option");
+
+  walletOption.addEventListener("click", () => {
+    walletOption.classList.add("selected");
+    contestOption.classList.remove("selected");
+  });
+
+  contestOption.addEventListener("click", () => {
+    contestOption.classList.add("selected");
+    walletOption.classList.remove("selected");
+  });
+
+  document
+    .getElementById("btn_receive_page")
+    .addEventListener("click", function () {
+      window.location.href = "select_crypto_recieve.html";
+    });
+
+  document
+    .getElementById("btn_send_page")
+    .addEventListener("click", function () {
+      window.location.href = "select_crypto_send.html";
+    });
+
+  document
+    .getElementById("btn_swap_page")
+    .addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
+});
