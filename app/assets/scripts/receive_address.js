97ed40ad3882c88eaf752189490abadc918b76c9
rey256 <medvedosandr@gmail.com>
2024-09-19 17:26:50 +0300

    .

--- app/assets/scripts/receive_address.js
+++ app/assets/scripts/receive_address.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const back = document.getElementById("back-button");
 
-  // back.addEventListener("click", function () {
-  //   window.location.href = "select_crypto_to_send.html";
-  // });
-
   back.addEventListener("click", function () {
-    window.location.href = "index.html";
+    window.location.href = "select_crypto_to_send.html";
   });
 });
