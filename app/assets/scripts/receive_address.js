fe861c9773844c3bf0703cccc4ec7ba004262567
rey256 <medvedosandr@gmail.com>
2024-10-01 19:04:59 +0300

    .

--- app/assets/scripts/receive_address.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const back = document.getElementById("back-button");
-
-  back.addEventListener("click", function () {
-    window.location.href = "select_crypto_to_send.html";
-  });
-});
--- app/assets/scripts/select_crypto_to_recieve.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const back = document.getElementById("back-button");
-
-  back.addEventListener("click", function () {
-    window.location.href = "index.html";
-  });
-});
