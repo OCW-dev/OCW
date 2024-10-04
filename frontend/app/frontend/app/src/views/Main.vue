084523f1c08b7be8eb4fe7258651aa07ed5cf2f6
Kostya <k.konstantin2212@gmail.com>
2024-10-05 00:36:45 +0300

    main

--- frontend/app/frontend/app/src/views/Main.vue
+++ frontend/app/frontend/app/src/views/Main.vue
@@ -52,95 +52,24 @@ import { RouterLink } from "vue-router";
       </RouterLink>
     </div>
 
-    <div class="flex-row-e">
-      <span class="eth">ETH</span><span class="dot">0.393039</span>
-      <div class="picture-eth"></div>
-      <span class="dollar-a">$2,784.59</span
-      ><span class="percentage">+0.63%</span
-      ><span class="dollar-b">$273,093</span>
-    </div>
-    <div class="flex-row-ec">
-      <span class="usdt">USDT</span><span class="text-11">50.6797</span>
-      <div class="img-8"></div>
-      <span class="text-12">$1.0037</span><span class="text-13">+0.63%</span
-      ><span class="text-14">$61,768</span>
-    </div>
-    <div class="section-5">
-      <span class="text-15">7.238</span>
-      <div class="box-2">
-        <div class="pic-6"></div>
-        <span class="text-16">USDC</span>
-      </div>
-    </div>
-    <div class="box-3">
-      <span class="text-17">$69.45</span>
-      <div class="section-6">
-        <span class="text-18">$0.9995</span><span class="text-19">-0.02%</span>
-      </div>
-    </div>
-    <div class="box-4">
-      <span class="text-1a">ETH</span><span class="text-1b">0.393039</span>
-      <div class="pic-7"></div>
-      <span class="text-1c">$2,784.59</span><span class="text-1d">+0.63%</span
-      ><span class="flex-row-b">$273,093</span>
-    </div>
-    <div class="div">
-      <span class="usdt-c">USDT</span><span class="dot-d">50.6797</span>
-      <div class="picture-type-png"></div>
-      <span class="dollar-e">$1.0037</span
-      ><span class="plus-percent">+0.63%</span
-      ><span class="flex-row-d">$61,768</span>
-    </div>
-    <div class="dot-f">
-      <span class="regroup-10">7.238</span>
-      <div class="regroup-11">
-        <div class="picture-usdc"></div>
-        <span class="usdc">USDC</span>
-      </div>
-    </div>
-    <div class="flex-row-eaa">
-      <span class="dollar-12">$69.45</span>
-      <div class="regroup-13">
-        <span class="dollar-14">$0.9995</span
-        ><span class="minus-percent">-0.02%</span>
-      </div>
-    </div>
-    <div class="flex-row-15">
-      <span class="eth-16">ETH</span><span class="dot-17">0.393039</span>
-      <div class="picture-eth-18"></div>
-      <span class="dollar-sign">$2,784.59</span
-      ><span class="percentage-up">+0.63%</span
-      ><span class="dollar-amount">$273,093</span>
-    </div>
-    <div class="flex-row-a">
-      <span class="usdt-19">USDT</span><span class="usdt-amount">50.6797</span>
-      <div class="picture-type"></div>
-      <span class="dollar-sign-1a">$1.0037</span
-      ><span class="percentage-up-1b">+0.63%</span
-      ><span class="dollar-amount-1c">$61,768</span>
-    </div>
-    <div class="flex-row-c">
-      <span class="number-amount">7.238</span>
-      <div class="regroup-1d">
-        <div class="picture-usdc-1e"></div>
-        <span class="usdc-1f">USDC</span>
-      </div>
-    </div>
-    <div class="flex-row-c-20">
-      <span class="dollar-amount-21">$69.45</span>
-      <div class="regroup-22">
-        <span class="dollar-sign-23">$0.9995</span
-        ><span class="percentage-down">-0.02%</span>
-      </div>
-    </div>
+    <CryptoList />
+    
   </div>
 </template>
 
 <script>
+import CryptoList from '../assets/components/CryptoList.vue';
+
 export default {
+  components: {
+    CryptoList
+  },
   data() {
     return {
       selected: 'Wallet',
+      cryptoData: {
+
+      }
     };
   },
   methods: {
