601a51031a404e85a87740b3e92bbf52e756b6d2
Kostya <k.konstantin2212@gmail.com>
2024-10-31 19:15:48 +0300

    main

--- frontend/bot/index.js
+++ frontend/bot/index.js
@@ -2,9 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 
-const token = process.env.token;
+const token = '7385369570:AAHeA_m0Zg65LO4bNm7fcisEUCngHyLS-8Q';
 
-const webAppurl = 'https://main--marvelous-madeleine-380246.netlify.app/';
+const webAppurl = 'https://67210e7fb7aba61dc652b94e--taupe-kitsune-b8b1f3.netlify.app/';
 
 
 console.log(token);
@@ -23,7 +23,10 @@ bot.on('message',async (msg) => {
             [{ text: 'Test', web_app: { url: webAppurl } }]
           ]
         }
-      });
+      });console.log('Bot started');
+console.log('Received message:', msg);
+console.log('Chat ID:', chatId);
+console.log('Text:', text);
   
     }
   } catch (error) {
